4e7b5a9f11ef2b9312c01c71687ece63adbbd252
Missing Lamperouge <locat@dhcp-10-250-212-37.harvard.edu>
2026-05-06 20:48:06 -0400

    Use mobile PaddleOCR models

--- app.py
+++ app.py
@@ -18,6 +18,8 @@ app = Flask(__name__)
 
 ocr = PaddleOCR(
     lang="ch",
+    text_detection_model_name="PP-OCRv5_mobile_det",
+    text_recognition_model_name="PP-OCRv5_mobile_rec",
     use_doc_orientation_classify=False,
     use_doc_unwarping=False,
     use_textline_orientation=False
